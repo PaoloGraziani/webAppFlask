684c77f48e3460a97f815c6049ea94920f963e2a
Lorenzo Genghini <l.genghini@campus.uniurb.it>
2021-09-03 21:18:43 +0200

    Sistemazioni varie
    Sistemazione file js, html e css

--- src/ManageDatabases/ApplicationDatabase/Order/sort.py
+++ src/ManageDatabases/ApplicationDatabase/Order/sort.py
@@ -2,7 +2,7 @@ from flask import jsonify
 
 from src.ManageDatabases.ApplicationDatabase.Agents.agents import agentOrderByID
 from src.ManageDatabases.ApplicationDatabase.Customer.customer import OrderByIDCustomer
-from src.ManageDatabases.SettingDatabase import connectDatabase, Application_HOST, Application_DATABASE, \
+from src.ManageDatabases.settingDatabase import connectDatabase, Application_HOST, Application_DATABASE, \
     Application_USERNAME, Application_PASSWORD
 
 
@@ -42,13 +42,13 @@ def sort_orders(par, role, username):
             agent_info = agentOrderByID(result[5], connection)
 
             for res in cust_info:
-                info = {'id': result[4], 'cust_name': res[1], 'cust_city': res[2], 'working_area': res[3],
+                info = {'id': res[0], 'cust_name': res[1], 'cust_city': res[2], 'working_area': res[3],
                         'cust_country': res[4], 'grade': float(res[5]), 'opening_amt': "{:.2f}".format(float(res[6])),
                         'receive_amt': "{:.2f}".format(float(res[7])), 'payment_amt': "{:.2f}".format(float(res[8])),
                         'outstanding_amt': "{:.2f}".format(float(res[9])), 'phone_no': res[10], 'agent_code': res[11]}
 
             for res in agent_info:
-                agent_info = {'agent_code': res[0], 'agent_name': res[1], "working_area": res[2],
+                agent_info = {'id': res[0], 'agent_name': res[1], "working_area": res[2],
                               "commission": "{:.2f}".format(float(res[3])), "phone_no": res[4], "country": res[5]}
 
             # num decimale, importOrder decimale advance_ord decimali
